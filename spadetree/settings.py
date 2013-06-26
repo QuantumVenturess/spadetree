a548b4f6144319ea42f03ae16ccca86ef85c4c62
Tommy Dang <quantumventuress@gmail.com>
2013-06-26 09:57:42 -0700

    debug true, compress false

--- spadetree/settings.py
+++ spadetree/settings.py
@@ -4,9 +4,9 @@ import os, platform, socket
 # Check environment
 if os.environ.get('MYSITE_PRODUCTION', False):
     # Production
-    DEBUG = TEMPLATE_DEBUG = False
+    DEBUG = TEMPLATE_DEBUG = True
     DEV                    = False
-    COMPRESS_ENABLED       = True
+    COMPRESS_ENABLED       = False
 else:
     # Development
     DEBUG = TEMPLATE_DEBUG = True
